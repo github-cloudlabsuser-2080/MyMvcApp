a71afa23ded56c067c29b7b2171888a9c0e8bf83
Melvin Berena <melvin.berena@rbc.com>
2024-09-26 16:22:10 +0000

    added controller

--- obj/Debug/net8.0/GitHub Copilot.AssemblyInfo.cs
+++ obj/Debug/net8.0/GitHub Copilot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHub Copilot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f501e12c97fc4f2071d0a27f4a40fcfdd4b18f8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18717b214dbc12474294258514bfec6d4876eab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHub Copilot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHub Copilot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
